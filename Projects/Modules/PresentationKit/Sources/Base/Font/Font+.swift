226f15dc0b698c6050ebf43eae6d07d104fc362b
UY <scutiuys@gmail.com>
2023-09-03 16:01:12 +0900

    [feat] design system  font, color 추가 #23

---
+++ Projects/Modules/PresentationKit/Sources/Base/Font/Font+.swift
@@ -0,0 +1,54 @@
+//
+//  Font+.swift
+//  PresentationKit
+//
+//  Created by 신의연 on 2023/08/26.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    class Head0 {
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 34)
+    }
+    
+    class Head1 {
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 28)
+    }
+    
+    class Head2 {
+        static let regular = PresentationKitFontFamily.Pretendard.regular.font(size: 22)
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 22)
+    }
+    
+    class Title1 {
+        static let regular = PresentationKitFontFamily.Pretendard.regular.font(size: 20)
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 20)
+    }
+    
+    class Title2 {
+        static let regular = PresentationKitFontFamily.Pretendard.regular.font(size: 18)
+        
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 18)
+    }
+    
+    class Body1 {
+        static let regular = PresentationKitFontFamily.Pretendard.regular.font(size: 16)
+        
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 16)
+    }
+    
+    class Body2 {
+        static let regular = PresentationKitFontFamily.Pretendard.regular.font(size: 14)
+        
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 14)
+    }
+    
+    class Body3 {
+        static let regular = PresentationKitFontFamily.Pretendard.regular.font(size: 12)
+        
+        static let semiBold = PresentationKitFontFamily.Pretendard.semiBold.font(size: 12)
+    }
+}

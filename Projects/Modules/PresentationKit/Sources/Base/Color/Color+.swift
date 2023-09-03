d58119ff8297a45299e4657e00a5a3c4776b21f8
ScutiUY <scutiuys@gmail.com>
2023-09-03 16:25:18 +0900

    Feature/23 design system (#31)
    * [feat]font 추가
    
    pretendard font 추가
    
    * [feat] design system  font, color 추가 #23
    
    * [feat] design system Font 수정
    
    코드 최적화

---
+++ Projects/Modules/PresentationKit/Sources/Base/Color/Color+.swift
@@ -0,0 +1,43 @@
+//
+//  Color+.swift
+//  PresentationKit
+//
+//  Created by 신의연 on 2023/08/26.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let main1 = Color(hex: 0xFF8F00, opacity: 1)
+    static let main2 = Color(hex: 0xFFB111, opacity: 1)
+    static let main3 = Color(hex: 0xFFD740, opacity: 1)
+    static let main4 = Color(hex: 0xFFE57F, opacity: 1)
+    static let main5 = Color(hex: 0xFFECB3, opacity: 1)
+    static let main6 = Color(hex: 0xFFF8E1, opacity: 1)
+    
+    static let ozWhite = Color(hex: 0xFFFFFF, opacity: 1)
+    static let mainLinear = Color(hex: 0xFF8F00, opacity: 1).gradient
+    
+    static let error = Color(hex: 0xFFF8E1, opacity: 1)
+    
+    static let orangeGray1 = Color(hex: 0x302D29, opacity: 1)
+    static let orangeGray2 = Color(hex: 0x534F4B, opacity: 1)
+    static let orangeGray3 = Color(hex: 0x736F6A, opacity: 1)
+    static let orangeGray4 = Color(hex: 0x88847F, opacity: 1)
+    static let orangeGray5 = Color(hex: 0xB2AEA9, opacity: 1)
+    static let orangeGray6 = Color(hex: 0xD0CBC6, opacity: 1)
+    static let orangeGray7 = Color(hex: 0xF1EDE8, opacity: 1)
+    static let orangeGray8 = Color(hex: 0xF6F2ED, opacity: 1)
+    static let orangeGray9 = Color(hex: 0xFBF7F2, opacity: 1)
+    static let orangeGray10 = Color(hex: 0xFFFCF6, opacity: 1)
+    
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((Int(hex) >> 16) & 0xff) / 255
+        let green = Double((Int(hex) >> 8) & 0xff) / 255
+        let blue = Double((Int(hex) >> 0) & 0xff) / 255
+        
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}

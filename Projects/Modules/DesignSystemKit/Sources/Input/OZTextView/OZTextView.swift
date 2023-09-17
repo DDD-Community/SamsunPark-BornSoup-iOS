4553a3d95872f9f24276745f125e2a536398ae9f
UY <scutiuys@gmail.com>
2023-09-17 19:49:56 +0900

    [feat] designSystem textView 구현

--- Projects/Modules/DesignSystemKit/Sources/Input/OZTextView/OZTextView.swift
+++ Projects/Modules/DesignSystemKit/Sources/Input/OZTextView/OZTextView.swift
@@ -9,16 +9,66 @@
 import SwiftUI
 
 struct OZTextView: View {
-    @State var text = ""
+    
+    enum Constant {
+        static let lineWidth: CGFloat = 1
+        static let cornerRadius: CGFloat = 10
+    }
+    @State private var fullText: String = "1"
+    @State private var placeholder: String = "d"
+    
     var body: some View {
-        VStack {
-            TextEditor(text: $text)
+        VStack(spacing: 0) {
+            ZStack(alignment: .leading) {
+                TextEditor(text: $fullText)
+                    .foregroundColor(.orangeGray1)
+                    .font(.Body1.regular)
+                    .padding(.top, 12)
+                    .padding(.horizontal, 16)
+                if fullText.isEmpty {
+                    TextEditor(text: $placeholder)
+                        .disabled(true)
+                        .foregroundColor(Color.gray)
+                        .font(.Body1.regular)
+                        .padding(.top, 12)
+                        .padding(.horizontal, 16)
+                }
+            }
+            
+            HStack(spacing: 0) {
+                Spacer()
+                Text("\(fullText.count)")
+                    .foregroundColor(.orangeGray1)
+                Text("/2000")
+                    .foregroundColor(.orangeGray5)
+            }
+            .font(.Body3.regular)
+            .padding(.top, 5)
+            .padding(.bottom, 12)
+            .padding(.horizontal, 12)
         }
+        .overlay(
+            Group {
+                RoundedRectangle(cornerRadius: Constant.cornerRadius)
+                    .stroke(
+                        Color.orangeGray5,
+                        lineWidth: Constant.lineWidth
+                    )
+            }
+        )
     }
 }
 
 struct OZTextView_Previews: PreviewProvider {
     static var previews: some View {
-        OZTextView()
+        VStack {
+            OZTextView()
+                .frame(height: 143)
+                .padding()
+            
+            OZTextView()
+                .frame(height: 167)
+                .padding()
+        }
     }
 }

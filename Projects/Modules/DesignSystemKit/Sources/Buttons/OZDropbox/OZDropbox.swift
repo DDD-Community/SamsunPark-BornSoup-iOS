0b000ee5f730a033a5bd78794da87a2cc3a90a5e
ScutiUY <scutiuys@gmail.com>
2023-09-17 22:53:16 +0900

    [feat] design system inputViews (#60)
    * [feat] design system input view 구성을 위한 기본 세팅
    
    * [feat] font swiftUI 변경
    
    * [feat] color error 변경
    
    * [feat] 새로운 이미지 추가
    
    * [feat] design system Input 구현
    
    * [chore] designSystem kit 추가에 따른 파일 경로 변경
    
    * [chore] designSystemKit 추가에 따른 presentationKit resource 정리
    
    * [chore] branch merge를 위한 Image 파일 경로 삭제
    
    * [feat] Image designSystem 변경
    
    * [feat] Dropbox invalidate 기능 변경
    
    * [feat] designSystem 변경에 따른 Image path 변경
    
    * [feat] input view path 변경
    
    * [feat] designSystem textView 구현
    
    * [feat] preview 예외처리 추가
    
    ---------
    
    Co-authored-by: bengHak <qkwl4678@naver.com>

---
+++ Projects/Modules/DesignSystemKit/Sources/Buttons/OZDropbox/OZDropbox.swift
@@ -0,0 +1,90 @@
+//
+//  OZDropbox.swift
+//  PresentationKit
+//
+//  Created by 신의연 on 2023/09/03.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import SwiftUI
+
+struct OZDropboxButtonStyle: ButtonStyle {
+    enum Constant {
+        static let lineWidth: CGFloat = 1
+        static let cornerRadius: CGFloat = 10
+    }
+    
+    var invalidation: Bool
+    
+    init(
+        invalidation: Bool = false
+    ) {
+        self.invalidation = invalidation
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .overlay(
+                Group {
+                    RoundedRectangle(cornerRadius: Constant.cornerRadius)
+                        .stroke(
+                            invalidation ? Color.init(hex: 0xF10000) : Color.orangeGray5,
+                            lineWidth: Constant.lineWidth
+                        )
+                }
+            )
+    }
+}
+
+struct OZDropbox: View {
+    
+    public var title: String
+    public var invalidation: Bool
+    public var action: (() -> Void)?
+    
+    init(
+        title: String,
+        invalidation: Bool = false,
+        action: (() -> Void)?
+    ) {
+        self.title = title
+        self.invalidation = invalidation
+        self.action = action
+    }
+    
+    var body: some View {
+        Button {
+            action?()
+        } label: {
+            HStack {
+                Text(title)
+                    .lineLimit(1)
+                    .font(.Body1.regular)
+                    .foregroundColor(.orangeGray5)
+                Spacer()
+                DesignSystemKitAsset.icCaredown22.swiftUIImage
+                    .renderingMode(.template)
+                    .foregroundColor(.orangeGray3)
+            }
+            .padding(12)
+            
+        }
+        .buttonStyle(
+            OZDropboxButtonStyle(invalidation: self.invalidation)
+        )
+    }
+}
+
+#if DEBUG
+struct OZDropbox_Previews: PreviewProvider {
+    static var previews: some View {
+        OZDropbox(
+            title: "행사를 선택해주세요",
+            invalidation: false
+        ) {
+            print("pressed")
+        }
+    }
+}
+#endif
---
+++ Projects/Modules/DesignSystemKit/Sources/Input/OZTextField/OZTextField.swift
@@ -0,0 +1,97 @@
+//
+//  OZTextField.swift
+//  PresentationKit
+//
+//  Created by 신의연 on 2023/09/03.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import SwiftUI
+
+struct OZTextFieldStyle: TextFieldStyle {
+    @Binding var text: String
+    @Binding var invalidation: Bool
+    @FocusState private var isFocused: Bool
+    
+    init(
+        text: Binding<String>,
+        invalidation: Binding<Bool>
+    ) {
+        self._text = text
+        self._invalidation = invalidation
+    }
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        HStack {
+            DesignSystemKitAsset.icSearch16.swiftUIImage
+                .renderingMode(.template)
+            
+            configuration
+                .focused($isFocused)
+                .onTapGesture {
+                    isFocused = true
+                }
+            Button {
+                text = ""
+            } label: {
+                if text.count > 0 && isFocused {
+                    DesignSystemKitAsset.icSearchDelete18.swiftUIImage
+                }
+            }
+        }
+        .onChange(of: invalidation) { newValue in
+            isFocused = false
+        }
+        .padding(10)
+    }
+}
+
+struct OZTextField: View {
+    
+    let title: any StringProtocol
+    @Binding var text: String
+    @Binding var invalidation: Bool
+    @State private var changingValue: Bool = false
+    
+    init(
+        title: any StringProtocol,
+        text: Binding<String>,
+        invalidation: Binding<Bool> = .constant(true)
+    ) {
+        self.title = title
+        self._text = text
+        self._invalidation = invalidation
+    }
+    
+    var body: some View {
+        TextField(title, text: $text) { changed in
+            changingValue = changed
+        }
+        .cornerRadius(6)
+        .overlay(
+            Group {
+                RoundedRectangle(cornerRadius: 23)
+                    .stroke(
+                        invalidation ? Color.error : Color.orangeGray1,
+                        lineWidth: 1
+                    )
+            }
+        )
+        .textFieldStyle(
+            OZTextFieldStyle(
+                text: $text,
+                invalidation: $invalidation
+            )
+        )
+    }
+}
+#if DEBUG
+struct OZTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            OZTextField(title: "플레이스 홀더", text: .constant("123"), invalidation: .constant(true))
+            OZTextField(title: "플레이스 홀더", text: .constant("123"), invalidation: .constant(false))
+        }
+    }
+}
+#endif

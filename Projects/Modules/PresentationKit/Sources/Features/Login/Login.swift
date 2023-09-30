03f455d8b2f1ecf494f0566879921eb1937dc401
bengHak <qkwl4678@naver.com>
2023-10-01 02:18:19 +0900

    feat: Implement LoginView #71

---
+++ Projects/Modules/PresentationKit/Sources/Features/Login/Login.swift
@@ -0,0 +1,32 @@
+//
+//  Login.swift
+//  PresentationKit
+//
+//  Created by 고병학 on 9/30/23.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import ComposableArchitecture
+
+import Foundation
+
+public struct Login: Reducer {
+    public init() {}
+    
+    public struct State: Equatable {
+        public init() {}
+    }
+    
+    public enum Action {
+        
+    }
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            default:
+                return .none
+            }
+        }
+    }
+}
---
+++ Projects/Modules/PresentationKit/Sources/Features/Login/LoginView.swift
@@ -0,0 +1,104 @@
+//
+//  LoginView.swift
+//  PresentationKit
+//
+//  Created by 고병학 on 9/30/23.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import ComposableArchitecture
+
+import SwiftUI
+
+fileprivate enum Constants {
+    enum Sizes {
+        static let logoWidth: CGFloat = 90.0
+        static let logoHeight: CGFloat = 90.0
+        static let snsLoginLogoSize: CGFloat = 18.0
+        static let snsLoginHorizontalSpace: CGFloat = 10.0
+        static let bottomPaddingOfLogo: CGFloat = 16.0
+        static let textBottomPadding: CGFloat = 12.0
+        static let signInLabelVerticalPadding: CGFloat = 16.0
+        static let containerHorizontalPadding: CGFloat = 16.0
+        static let loginButtonBottomPadding: CGFloat = 8.0
+        static let buttonCornerRadius: CGFloat = 100.0
+    }
+    enum Colors {
+        static let kakaoYellow: Color = Color(red: 1, green: 0.9, blue: 0)
+        
+    }
+    enum Strings {
+        static let appName: String = "오 · 전"
+        static let appSubtitle: String = "오늘을 전통문화로 채우다"
+        static let kakaoLogin: String = "카카오로 로그인"
+        static let appleLogin: String = "Apple로 로그인"
+        static let lookAround: String = "둘러보기"
+    }
+}
+
+public struct LoginView: View {
+    
+    let store: StoreOf<Login>
+    
+    public init(store: StoreOf<Login>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { _ in
+            VStack(spacing: 0) {
+                VStack {
+                    Spacer()
+                    DesignSystemKitAsset.icOzeonLogo.swiftUIImage
+                        .resizable()
+                        .frame(
+                            width: Constants.Sizes.logoWidth,
+                            height: Constants.Sizes.logoHeight
+                        )
+                        .padding(.bottom, Constants.Sizes.bottomPaddingOfLogo)
+                    Text(Constants.Strings.appName)
+                        .font(Font.Head1.semiBold)
+                        .padding(.bottom, Constants.Sizes.textBottomPadding)
+                    Text(Constants.Strings.appSubtitle)
+                        .font(Font.Body1.regular)
+                        .foregroundStyle(Color.orangeGray5)
+                    Spacer()
+                }
+                
+                SNSLoginButton(snsType: .KAKAO) {
+                    print("카카오로 로그인")
+                }
+                .padding(.horizontal, Constants.Sizes.containerHorizontalPadding)
+                .padding(.bottom, Constants.Sizes.loginButtonBottomPadding)
+                
+                SNSLoginButton(snsType: .APPLE) {
+                    print("애플로 로그인")
+                }
+                .padding(.horizontal, Constants.Sizes.containerHorizontalPadding)
+                .padding(.bottom, Constants.Sizes.loginButtonBottomPadding)
+
+                Button(action: {
+                    print("둘러보기")
+                }, label: {
+                    Text(Constants.Strings.lookAround)
+                        .font(.Title2.regular)
+                        .foregroundStyle(Color.orangeGray5)
+                })
+                .padding(.vertical, Constants.Sizes.loginButtonBottomPadding)
+            }
+            .navigationBarBackButtonHidden()
+        }
+    }
+}
+
+#if DEBUG
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView(
+            store: Store(initialState: Login.State()) {
+                Login()
+            }
+        )
+    }
+}
+#endif

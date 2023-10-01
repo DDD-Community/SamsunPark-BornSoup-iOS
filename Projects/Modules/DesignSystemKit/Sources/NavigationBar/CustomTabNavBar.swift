d830dc08e345adeb254de0557852523bafe17156
bengHak <qkwl4678@naver.com>
2023-10-01 17:42:54 +0900

    feat: Implement CustomTabNavBar #24

---
+++ Projects/Modules/DesignSystemKit/Sources/NavigationBar/CustomTabNavBar.swift
@@ -0,0 +1,106 @@
+//
+//  CustomTabNavBar.swift
+//  DesignSystemKit
+//
+//  Created by 고병학 on 10/1/23.
+//  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
+//
+
+import SwiftUI
+
+public struct TextButtonContent {
+    public init(text: String, action: @escaping () -> Void) {
+        self.text = text
+        self.action = action
+    }
+    
+    public let text: String
+    public let action: () -> Void
+}
+
+public struct ImageButtonContent {
+    public init(image: Image, action: @escaping () -> Void) {
+        self.image = image
+        self.action = action
+    }
+    
+    public let image: Image
+    public let action: () -> Void
+}
+
+public struct CustomTabNavBar: View {
+    
+    public init(
+        tabButtons: [TextButtonContent],
+        selectedTab: Int,
+        buttons: [ImageButtonContent]
+    ) {
+        self.tabButtons = tabButtons
+        self.selectedTab = selectedTab
+        self.buttons = buttons
+    }
+    
+    let selectedTab: Int
+    let tabButtons: [TextButtonContent]
+    let buttons: [ImageButtonContent]
+    
+    public var body: some View {
+        ZStack {
+            HStack(spacing: 16) {
+                ForEach(Array(tabButtons.enumerated()), id: \.0) { (index, content) in
+                    Text(content.text)
+                        .font(
+                            selectedTab == index ? .Head2.semiBold
+                            : .Head2.regular
+                        )
+                        .foregroundColor(
+                            selectedTab == index ? Color.orangeGray1
+                            : Color.orangeGray5
+                        )
+                }
+                Spacer()
+            }
+            HStack(spacing: 16) {
+                Spacer()
+                ForEach(Array(buttons.enumerated()), id: \.0) { (_, content) in
+                    Button(action: content.action, label: {
+                        content.image
+                            .frame(width: 24, height: 24)
+                    })
+                }
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 17)
+        .padding(.bottom, 16)
+    }
+}
+
+#if DEBUG
+struct CustomTabNavBar_PreViews: PreviewProvider {
+    static var previews: some View {
+        CustomTabNavBar(
+            tabButtons: [
+                TextButtonContent(text: "최신순", action: {}),
+                TextButtonContent(text: "인기순", action: {})
+            ],
+            selectedTab: 1,
+            buttons: [
+                ImageButtonContent(
+                    image: DesignSystemKitAsset.icShare24.swiftUIImage,
+                    action: {}
+                ),
+                ImageButtonContent(
+                    image: DesignSystemKitAsset.icSearch24.swiftUIImage,
+                    action: {}
+                ),
+                ImageButtonContent(
+                    image: DesignSystemKitAsset.icFilter24.swiftUIImage,
+                    action: {}
+                )
+            ]
+        )
+    
+    }
+}
+#endif

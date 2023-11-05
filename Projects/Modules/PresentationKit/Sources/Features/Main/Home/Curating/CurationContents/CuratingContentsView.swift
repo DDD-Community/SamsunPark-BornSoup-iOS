04248d9d0154d9549a2c6887f98c37728218451f
UY <scutiuys@gmail.com>
2023-11-05 21:45:56 +0900

    [feat] curating 세부 view 변경
    - 기존 구조에서 store를 제거하고 view만 사용합니다.
    - 해당view에서 bl이 들어갈 요소가 없음

--- Projects/Modules/PresentationKit/Sources/Features/Main/Home/Curating/CurationContents/CuratingContentsView.swift
+++ Projects/Modules/PresentationKit/Sources/Features/Main/Home/Curating/CurationContents/CuratingContentsView.swift
@@ -1,5 +1,5 @@
 //
-//  CurationContentsView.swift
+//  CuratingContentsView.swift
 //  PresentationKit
 //
 //  Created by 신의연 on 11/5/23.
@@ -12,74 +12,65 @@ import ComposableArchitecture
 import DomainKit
 import Kingfisher
 
-struct CurationContentsView: View {
-    let store: StoreOf<CurationContent>
+struct CuratingContentsView: View {
+    let contents: PreviewContentsModel
     
     var body: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            ZStack {
-                KFImage(URL(string: "https://picsum.photos/400/600"))
-                    .resizable()
-                
-                    .clipped()
-                    .clipShape(
-                        RoundedRectangle(
-                            cornerRadius: Constants.cornerRadius
-                        )
+        ZStack {
+            KFImage(URL(string: contents.thumbnail[0]))
+                .resizable()
+            
+                .clipped()
+                .clipShape(
+                    RoundedRectangle(
+                        cornerRadius: Constants.cornerRadius
                     )
-                RoundedRectangle(cornerRadius: Constants.cornerRadius)
-                    .fill(
-                        Gradient(colors: [.clear, .black])
-                            .opacity(0.4)
-                        )
-                    
+                )
+            RoundedRectangle(cornerRadius: Constants.cornerRadius)
+                .fill(
+                    Gradient(colors: [.clear, .black])
+                        .opacity(0.4)
+                )
+            
+            VStack(alignment: .leading, spacing: 0) {
+                Spacer()
+                
+                
                 VStack(alignment: .leading, spacing: 0) {
-                    Spacer()
-                    
-                    
-                    VStack(alignment: .leading, spacing: 0) {
-                        //TODO: - tag or badge 만들어서 merge 다시 해야 함
-                        Text(viewStore.previewContents.category.rawValue)
+                    //TODO: - tag or badge 만들어서 merge 다시 해야 함
+                    Text(contents.category.rawValue)
                         .foregroundColor(Color.ozWhite)
-                        
-                        Text(viewStore.previewContents.title)
-                            .font(.Head2.semiBold)
-                            .foregroundColor(Color.ozWhite)
-                            .padding(.top, 20)
-                        
-                        HStack(alignment: .center, spacing: 8) {
-                            Text("\(viewStore.previewContents.area), \(viewStore.previewContents.place)")
-                            Rectangle()
-                                .frame(width: 1, height: 16)
-                            Text("\(viewStore.previewContents.startDate) ~ \(String(viewStore.previewContents.endDate.split(separator:".")[1])).\(String(viewStore.previewContents.endDate.split(separator: ".")[2]))")
-                                
-                        }
-                        .font(.Body1.regular)
+                    
+                    Text(contents.title)
+                        .font(.Head2.semiBold)
                         .foregroundColor(Color.ozWhite)
                         .padding(.top, 20)
+                    
+                    HStack(alignment: .center, spacing: 8) {
+                        Text("\(contents.area), \(contents.place)")
+                        Rectangle()
+                            .frame(width: 1, height: 16)
+                        Text("\(contents.startDate) ~ \(String(contents.endDate.split(separator:".")[1])).\(String(contents.endDate.split(separator: ".")[2]))")
+                        Spacer()
                     }
-                    .padding(.bottom, 30)
-                    .padding(.horizontal, 30)
+                    .font(.Body1.regular)
+                    .foregroundColor(Color.ozWhite)
+                    .padding(.top, 20)
                 }
+                .padding(.bottom, 30)
+                .padding(.horizontal, 30)
             }
         }
     }
 }
 
 #Preview {
-    CurationContentsView(
-        store: Store(
-            initialState: CurationContent.State(
-                previewContents: PreviewContentsModel.mock
-            ),
-            reducer: {
-                CurationContent()
-            }
-        )
+    CuratingContentsView(
+        contents: PreviewContentsModel.mock1
     )
 }
 
-extension CurationContentsView {
+extension CuratingContentsView {
     enum Constants {
         static let cornerRadius: CGFloat = 30
     }

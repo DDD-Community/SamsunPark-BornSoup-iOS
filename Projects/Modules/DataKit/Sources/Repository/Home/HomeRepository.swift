da33b54ad6ec9214c4c70d1cf39e572e9a08ced0
UY <scutiuys@gmail.com>
2023-11-19 16:54:40 +0900

    [feat] home repository 생성

--- Projects/Modules/DataKit/Sources/Repository/Home/HomeRepository.swift
+++ Projects/Modules/DataKit/Sources/Repository/Home/HomeRepository.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
 //
 
+import Alamofire
+import DomainKit
+import NetworkKit
+import ComposableArchitecture
+
 import Foundation
+
+public struct HomeRepository: HomeRepositoryProtocol {
+    
+    private let baseAPIClient: BaseAPIClient = .init()
+    
+    public init() {}
+    
+    public func requestContentsList() async throws -> DomainKit.ContentsListResponse {
+        let request = try HomeAPIService.contentsList.asQueryURLRequest()
+        request.log()
+        let (data, response) = try await URLSession.shared.data(for: request)
+        //TODO: - response error 처리
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(ContentsListResponse.self, from: data)
+        } catch let error {
+            throw error
+        }
+    }
+    
+    public func requestContentsList(page: Int, size: Int) async throws -> ContentsListResponse? {
+        return nil
+    }
+    
+    public func requestContentsListOrderByReviewCount() async throws -> ContentsListResponse? {
+        return nil
+    }
+}

ea77d67f284329d4afa5c9e4638c2eef4e60e549
UY <scutiuys@gmail.com>
2023-11-25 22:00:14 +0900

    [feat] implement content detail usecase

--- Projects/Modules/DomainKit/Sources/ContentsDetail/UseCase/ContentsDetailUseCase.swift
+++ Projects/Modules/DomainKit/Sources/ContentsDetail/UseCase/ContentsDetailUseCase.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
 //
 
+import ComposableArchitecture
+import CoreKit
+import DIKit
+
 import Foundation
+
+public protocol ContentsDetailUseCaseProtocol {
+    func requestContentsDetail(seq: String) async throws -> ContentsResponse
+}
+
+public struct ContentsDetailUseCase: ContentsDetailUseCaseProtocol {
+    private let repository: ContentsDetailRepositoryProtocol
+    
+    public init(repository: ContentsDetailRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    public func requestContentsDetail(seq: String) async throws -> ContentsResponse {
+        return try await repository.requestContentsDetail(seq: seq)
+    }
+}
+
+extension ContentsDetailUseCase: DependencyKey {
+    public static let liveValue: ContentsDetailUseCase = Self(repository: DIContainer.container.resolve(ContentsDetailRepositoryProtocol.self)!)
+}
+
+extension DependencyValues {
+    public var contentsDetailUseCase: ContentsDetailUseCase {
+        get { self[ContentsDetailUseCase.self] }
+        set { self[ContentsDetailUseCase.self] = newValue }
+    }
+}

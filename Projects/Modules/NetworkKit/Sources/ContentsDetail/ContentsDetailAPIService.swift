07e9e8e0e8ae79bb4f506af749b4c17a4d42ce28
UY <scutiuys@gmail.com>
2023-11-25 22:01:13 +0900

    [feat] implement contents detail apiservice

--- Projects/Modules/NetworkKit/Sources/ContentsDetail/ContentsDetailAPIService.swift
+++ Projects/Modules/NetworkKit/Sources/ContentsDetail/ContentsDetailAPIService.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
 //
 
+import Alamofire
+
 import Foundation
+
+public enum ContentsDetailAPIService {
+    case contentsDetail(String)
+}
+
+extension ContentsDetailAPIService: TargetType {
+    public var path: String {
+        switch self {
+        case .contentsDetail:
+            return "/InfoDetail"
+        }
+    }
+    
+    public var method: HTTPMethod {
+        switch self {
+        case .contentsDetail:
+            return .get
+        }
+    }
+    
+    public var parameters: Parameters? {
+        switch self {
+        case .contentsDetail(let seq):
+            let parameter: [String: Any] = [
+                "seq": seq
+            ]
+            return parameter
+        }
+    }
+    
+    public var headers: HTTPHeaders? {
+        return [
+            HTTPHeader.contentType("application/json"),
+            HTTPHeader.acceptLanguage(Locale.current.identifier)
+        ]
+    }
+}

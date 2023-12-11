f13307aa3cf70652a9a109de49d0cbf447fd3615
UY <scutiuys@gmail.com>
2023-12-11 19:27:26 +0900

    [feat] 주변정보 구현

--- Projects/Modules/PresentationKit/Sources/Features/Neighborhood/Neighborhood.swift
+++ Projects/Modules/PresentationKit/Sources/Features/Neighborhood/Neighborhood.swift
@@ -6,4 +6,189 @@
 //  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
 //
 
+import ComposableArchitecture
+
 import Foundation
+import MapKit
+import CoreLocation
+import DomainKit
+import SwiftUI
+
+public struct Neighborhood: Reducer {
+    public struct SelectableCategory: Equatable {
+        var category: ContentsType
+        var isSelected: Bool
+        
+        func getContentsImage() -> Image {
+            switch category {
+            case .review:
+                return DesignSystemKitAsset.icOldPalace24.swiftUIImage
+            case .location:
+                return DesignSystemKitAsset.icOldPalace24.swiftUIImage
+            case .palace:
+                return DesignSystemKitAsset.icOldPalace24.swiftUIImage
+            case .music:
+                return DesignSystemKitAsset.icMusic24.swiftUIImage
+            case .dance:
+                return DesignSystemKitAsset.icDance24.swiftUIImage
+            case .craft:
+                return DesignSystemKitAsset.icCrafts24.swiftUIImage
+            case .experience:
+                return DesignSystemKitAsset.icExperience24.swiftUIImage
+            case .museum:
+                return DesignSystemKitAsset.icMuseum24Colored.swiftUIImage
+            case .artGallery:
+                return DesignSystemKitAsset.icArtGallery24Colored.swiftUIImage
+            case .performance:
+                return DesignSystemKitAsset.icExperience24.swiftUIImage
+            case .etc:
+                return DesignSystemKitAsset.icSetting24.swiftUIImage
+            }
+        }
+    }
+    
+    public struct State: Equatable {
+        public static func == (lhs: Neighborhood.State, rhs: Neighborhood.State) -> Bool {
+            return lhs.region.center.latitude == rhs.region.center.latitude
+            && lhs.region.center.longitude == rhs.region.center.latitude
+            && lhs.region.span.latitudeDelta == rhs.region.span.latitudeDelta
+            && lhs.region.span.longitudeDelta == rhs.region.span.longitudeDelta
+        }
+        
+        var region: MKCoordinateRegion = .init(
+            center: .gangnam,
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.05
+            )
+        )
+        var visibleRect: MKMapRect = .init()
+        var contentsAnnotationList: [ContentsAnnotation] = []
+        var selectableCategories: [SelectableCategory] = []
+        var text: String = ""
+        
+        var search: Search.State = .init(text: "", recentSearches: [], searchResultList: .init(contentsList: []))
+    }
+    
+    public enum Action {
+        case onAppear
+        case regionChanged(MKCoordinateRegion)
+        case visibleRectChanged(MKMapRect)
+        case searchTextChanged(String)
+        case categorySelected(Int)
+        case contentsListChanged(ContentsType)
+        
+        case search(Search.Action)
+    }
+    
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .none
+                
+            case .regionChanged(let region):
+                state.region = region
+                print(region)
+                return .none
+                
+            case .visibleRectChanged(let rect):
+                print(rect.minX, rect.maxX, rect.minY, rect.maxY)
+                return .none
+                
+            case let .searchTextChanged(text):
+                state.text = text
+                return .none
+                
+            case let .categorySelected(index):
+                for (idx, _) in state.selectableCategories.enumerated() {
+                    if idx == index {
+                        state.selectableCategories[idx].isSelected.toggle()
+                    } else {
+                        state.selectableCategories[idx].isSelected = false
+                    }
+                }
+                return .send(.contentsListChanged(state.selectableCategories[index].isSelected ? state.selectableCategories[index].category : ContentsType.location))
+                
+            case .contentsListChanged:
+                return .none
+                
+            case .search:
+                return .none
+            }
+        }
+        Scope(state: \.search, action: /Action.search) {
+            Search()
+        }
+    }
+}
+
+extension Neighborhood {
+    static let placeholder = Neighborhood.State(
+        contentsAnnotationList: [
+            ContentsAnnotation(
+                name: "gangnam",
+                coordinate: .init(latitude: 37.49797616957218, longitude: 127.02764665816525),
+                category: .artGallery
+            ),
+            ContentsAnnotation(
+                name: "gyeongbokgung",
+                coordinate: .init(latitude: 37.578611, longitude: 126.977222),
+                category: .palace
+            ),
+            ContentsAnnotation(
+                name: "국립현대미술관",
+                coordinate: .init(latitude: 37.578628, longitude: 126.980261),
+                category: .artGallery
+            ),
+            
+            ContentsAnnotation(
+                name: "Lola's Place",
+                coordinate: .init(latitude: -32.4, longitude: 115.7),
+                category: .location
+            )
+        ], selectableCategories: [
+            SelectableCategory(category: .artGallery, isSelected: false),
+            SelectableCategory(category: .craft, isSelected: false),
+            SelectableCategory(category: .dance, isSelected: false)
+        ])
+}
+
+extension CLLocationCoordinate2D {
+    static let gangnam = CLLocationCoordinate2D(latitude: 37.49797616957218, longitude: 127.02764665816525)
+    static let gyeongbokgung = CLLocationCoordinate2D(latitude: 37.578611, longitude: 126.977222)
+}
+
+public struct ContentsAnnotation: Identifiable {
+    public var id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+    let category: ContentsType
+    
+    func getCategoryImage() -> Image {
+        switch category {
+        case .review:
+            return DesignSystemKitAsset.icOldPalace24Colored.swiftUIImage
+        case .location:
+            return DesignSystemKitAsset.icOldPalace24Colored.swiftUIImage
+        case .palace:
+            return DesignSystemKitAsset.icOldPalace24Colored.swiftUIImage
+        case .music:
+            return DesignSystemKitAsset.icMusic24Colored.swiftUIImage
+        case .dance:
+            return DesignSystemKitAsset.icDance24Colored.swiftUIImage
+        case .craft:
+            return DesignSystemKitAsset.icCrafts24Colored.swiftUIImage
+        case .experience:
+            return DesignSystemKitAsset.icExperience24Colored.swiftUIImage
+        case .museum:
+            return DesignSystemKitAsset.icMuseum24Colored.swiftUIImage
+        case .artGallery:
+            return DesignSystemKitAsset.icArtGallery24Colored.swiftUIImage
+        case .performance:
+            return DesignSystemKitAsset.icExperience24Colored.swiftUIImage
+        case .etc:
+            return DesignSystemKitAsset.icSetting24.swiftUIImage
+        }
+    }
+}
--- Projects/Modules/PresentationKit/Sources/Features/Neighborhood/NeighborhoodView.swift
+++ Projects/Modules/PresentationKit/Sources/Features/Neighborhood/NeighborhoodView.swift
@@ -6,4 +6,96 @@
 //  Copyright © 2023 kr.ddd.ozeon. All rights reserved.
 //
 
-import Foundation
+import ComposableArchitecture
+
+import SwiftUI
+import MapKit
+import CoreLocation
+import DesignSystemKit
+
+struct NeighborhoodView: View {
+    let store: StoreOf<Neighborhood>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            if #available(iOS 17.0, *) {
+                if true {
+                    ZStack {
+                        OZMapView(
+                            region: viewStore.binding(
+                                get: \.region,
+                                send: Neighborhood.Action.regionChanged
+                            ),
+                            visibleRect: viewStore.binding(
+                                get: \.visibleRect,
+                                send: Neighborhood.Action.visibleRectChanged
+                            ),
+                            annotations: viewStore.contentsAnnotationList
+                        )
+                        .ignoresSafeArea()
+                        
+                        VStack {
+                            OZTextField(
+                                title: "콘텐츠명이나 지역구를 입력해보세요.",
+                                text: viewStore.binding(
+                                    get: \.text,
+                                    send: Neighborhood.Action.searchTextChanged
+                                )
+                            )
+                            .background(Color.ozWhite)
+                            .cornerRadius(23)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 16)
+                            
+                            ScrollView(.horizontal) {
+                                HStack {
+                                    ForEach(Array(viewStore.selectableCategories.enumerated()), id: \.0) { index ,selectableCategories in
+                                        HStack {
+                                            ChipButton(
+                                                text: "\(selectableCategories.category.rawValue)",
+                                                leadingImage: selectableCategories.getContentsImage(),
+                                                trailingImage: nil,
+                                                isSelected: selectableCategories.isSelected
+                                            )
+                                        }
+                                        .background(Color.ozWhite)
+                                        .clipShape(RoundedRectangle(cornerRadius: 23))
+                                        .onTapGesture {
+                                            print(index)
+                                            viewStore.send(.categorySelected(index))
+                                        }
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 16)
+                            Spacer()
+                        }
+                    }
+                    .onAppear {
+                        viewStore.send(.onAppear)
+                    }
+                } else {
+                    SearchView(
+                        store: self.store.scope(
+                            state: \.search,
+                            action: Neighborhood.Action.search
+                        )
+                    )
+                }
+            } else {
+                Text("unavaible")
+            }
+        }
+    }
+}
+
+#Preview {
+    NeighborhoodView(
+        store: Store(
+            initialState: Neighborhood.placeholder,
+            reducer: {
+                Neighborhood()
+            }
+        )
+    )
+}
